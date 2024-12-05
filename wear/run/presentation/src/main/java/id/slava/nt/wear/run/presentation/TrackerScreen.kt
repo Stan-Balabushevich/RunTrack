e30a553f8c42136a7c8049a0cf804e00a84220e9
StanHigh <stasntnew@gmail.com>
2024-12-05 20:57:56 +0100

    Keeping the watch screen active with ambient mode  implemented

--- wear/run/presentation/src/main/java/id/slava/nt/wear/run/presentation/TrackerScreen.kt
+++ wear/run/presentation/src/main/java/id/slava/nt/wear/run/presentation/TrackerScreen.kt
@@ -47,6 +47,8 @@ import id.slava.nt.core.presentation.ui.ObserveAsEvents
 import id.slava.nt.core.presentation.ui.formatted
 import id.slava.nt.core.presentation.ui.toFormattedHeartRate
 import id.slava.nt.core.presentation.ui.toFormattedKm
+import id.slava.nt.wear.run.presentation.ambient.AmbientObserver
+import id.slava.nt.wear.run.presentation.ambient.ambientMode
 import id.slava.nt.wear.run.presentation.components.RunDataCard
 import org.koin.androidx.compose.koinViewModel
 
@@ -124,21 +126,21 @@ private fun TrackerScreen(
         permissionLauncher.launch(permissions.toTypedArray())
     }
 
-//    AmbientObserver(
-//        onEnterAmbient = {
-//            onAction(TrackerAction.OnEnterAmbientMode(it.burnInProtectionRequired))
-//        },
-//        onExitAmbient = {
-//            onAction(TrackerAction.OnExitAmbientMode)
-//        }
-//    )
+    AmbientObserver(
+        onEnterAmbient = {
+            onAction(TrackerAction.OnEnterAmbientMode(it.burnInProtectionRequired))
+        },
+        onExitAmbient = {
+            onAction(TrackerAction.OnExitAmbientMode)
+        }
+    )
 
     if (state.isConnectedPhoneNearby) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background),
-//                .ambientMode(state.isAmbientMode, state.burnInProtectionRequired),
+                .background(MaterialTheme.colorScheme.background)
+                .ambientMode(state.isAmbientMode, state.burnInProtectionRequired),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
