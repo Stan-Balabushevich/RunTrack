8d074b9e1c50d3ea09577a6c624e3218dc679a75
StanHigh <stasntnew@gmail.com>
2024-12-05 19:33:06 +0100

    Keeping the watch app alive in the background implemented

--- wear/app/src/main/java/id/slava/nt/wear/app/presentation/MainActivity.kt
+++ wear/app/src/main/java/id/slava/nt/wear/app/presentation/MainActivity.kt
@@ -8,13 +8,8 @@ package id.slava.nt.wear.app.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.wear.compose.material3.Button
-import androidx.wear.compose.material3.Text
+import id.slava.nt.core.notification.ActiveRunService
 import id.slava.nt.core.presentation.designsystem_wear.PlrunTheme
 import id.slava.nt.wear.run.presentation.TrackerScreenRoot
 
@@ -29,7 +24,19 @@ class MainActivity : ComponentActivity() {
 
             PlrunTheme {
                 TrackerScreenRoot(
-                    onServiceToggle = {}
+                    onServiceToggle = { shouldStartRunning ->
+                        if(shouldStartRunning) {
+                            startService(
+                                ActiveRunService.createStartIntent(
+                                    applicationContext, this::class.java
+                                )
+                            )
+                        } else {
+                            startService(
+                                ActiveRunService.createStopIntent(applicationContext)
+                            )
+                        }
+                    }
                 )
             }
         }
