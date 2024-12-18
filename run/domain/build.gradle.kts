b0dc43a0e51eb01b2b805bd63f98fa161d24ebff
StanHigh <stasntnew@gmail.com>
2024-12-18 14:42:23 +0100

    unit test for RunningTracker testing flow and coroutines implemented

--- run/domain/build.gradle.kts
+++ run/domain/build.gradle.kts
@@ -6,4 +6,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(projects.core.domain)
     implementation(projects.core.connectivity.domain)
+    testImplementation(projects.core.test)
+//    testImplementation(libs.coroutines.test)
+//    testImplementation(libs.turbine)
 }
